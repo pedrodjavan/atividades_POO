c2622b79e560890b8534af29c7f3c25437be5b00
pedrodjavan <pedrolucasnunesferreira36@gmail.com>
2026-06-17 17:15:55 -0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,14 @@
+# QUESTAO 01
+nome = input("Digite seu primeiro nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+nome_completo = nome + " " + sobrenome
+print(f"Bem-vindo(a), {nome_completo}")
+
+
+# Questao 02
+i = input("Digite uma frase: ")
+print(f"Espaços em branco: {i.count(" ")}")
+
+
+# Questao 03

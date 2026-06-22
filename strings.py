fd1844d5bdac01da59c5cfd0a7462adbd3b0b610
pedrodjavan <pedrolucasnunesferreira36@gmail.com>
2026-06-22 14:16:35 -0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -12,3 +12,21 @@ print(f"Espaços em branco: {i.count(" ")}")
 
 
 # Questao 03
+nome =  input("Informe o nome: ")
+
+for cont in range(len(nome)):
+    print(nome[0:cont  + 1])
+  
+# Questao 04
+numero = (input("Diga-me o numero: "))
+if len(numero) == 8:
+    print(f"9{numero[:4]}-{numero[4:9]}")
+
+elif len(numero) < 8:
+    print("Num pode!")
+
+else:
+    if numero[0] == '9':
+        print(f"{numero[:5]}-{numero[5:9]}")
+    else:
+        print("Num pode!")

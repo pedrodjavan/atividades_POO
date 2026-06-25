c472c023e133bde8c8bcd5e0b4ffaa024e8ed193
pedrodjavan <pedrolucasnunesferreira36@gmail.com>
2026-06-25 14:55:55 -0300

    Update listas.py

--- listas.py
+++ listas.py
@@ -25,3 +25,90 @@ for i in range(0, p):
 
 
 
+# questao 02
+urls = [
+    "www.reddit.com",
+    "www.yahoo.com",
+    "www.github.com",
+    "www.google.com"
+]
+
+dominios = [url[4:-4] for url in urls]
+
+print(dominios)
+
+
+# questao 03
+from random import randint
+
+lista = [randint(-100, 100) for _ in range(20)]
+
+lista_ordenada = sorted(lista)
+
+indice_maior = lista.index(max(lista))
+indice_menor = lista.index(min(lista))
+
+soma = sum(lista)
+media = soma / len(lista)
+
+print("Lista ordenada:", lista_ordenada)
+print("Lista original:", lista)
+print("Índice do maior valor:", indice_maior)
+print("Índice do menor valor:", indice_menor)
+print("Soma dos valores:", soma)
+print("Média dos valores:", media)
+
+# questao 04
+
+lista_1 = list(map(int, input("Diga-me os números da primeira lista: ").split()))
+lista_2 = list(map(int, input("Diga-me os números da segunda lista: ").split()))
+
+lista_3 = []
+menor_tamanho = min(len(lista_1), len(lista_2))
+
+for i in range(menor_tamanho):
+    lista_3.append(lista_1[i])
+    lista_3.append(lista_2[i])
+
+if len(lista_1) > len(lista_2):
+    lista_3.extend(lista_1[menor_tamanho:])
+else:
+    lista_3.extend(lista_2[menor_tamanho:])
+
+print("Lista intercalada:", lista_3)
+
+
+
+# questao 05
+
+from random import randint
+
+lista_1 = [randint(0, 50) for _ in range(20)]
+lista_2 = [randint(0, 50) for _ in range(20)]
+i = sorted(list(set(lista_1) & set(lista_2)))
+
+print("Lista 1:", lista_1)
+print("Lista 2:", lista_2)
+print("Interseção:", i)
+
+
+
+# questao 06 
+
+não consegui fazer essa e questão 7 :(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
